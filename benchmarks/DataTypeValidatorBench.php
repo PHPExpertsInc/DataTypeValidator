f8f93c0cdd109dc38ce864d29b5a51b9769b79d1
Theodore R. Smith <theodore@phpexperts.pro>
2019-05-12 13:08:48 -0500

    Added the license header.

--- benchmarks/DataTypeValidatorBench.php
+++ benchmarks/DataTypeValidatorBench.php
@@ -1,4 +1,16 @@
-<?php
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of DataTypeValidator, a PHP Experts, Inc., Project.
+ *
+ * Copyright © 2019 PHP Experts, Inc.
+ * Author: Theodore R. Smith <theodore@phpexperts.pro>
+ *  GPG Fingerprint: 4BF8 2613 1C34 87AC D28F  2AD8 EB24 A91D D612 5690
+ *  https://www.phpexperts.pro/
+ *  https://github.com/phpexpertsinc/DataTypeValidator
+ *
+ * This file is licensed under the MIT License.
+ */
 
 use PHPExperts\DataTypeValidator\DataTypeValidator;
 use PHPExperts\DataTypeValidator\InvalidDataTypeException;
